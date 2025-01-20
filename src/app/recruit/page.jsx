7ee73ad74ebd211be6df236d918da997299694f5
pwc2002 <pwc2002@naver.com>
2025-01-20 14:56:35 +0900

    Feat: 모집 폼 페이지 틀 생성 #8

---
+++ src/app/recruit/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+
+export default function Recruit() {
+
+  return (
+    <>
+        <div className="flex flex-col w-full h-full pl-[306px] pt-[132px]">
+            <p className="text-white text-4xl font-bold">GDGoC INHA 25-1 멤버에 지원해보세요</p>
+            <div className="flex flex-row w-full h-full mt-[90px]">
+
+            </div>
+        </div>
+    </>
+  );
+}
