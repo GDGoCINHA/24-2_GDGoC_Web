f41460d0ee4b23472fefca2dbf1ebc46c68ee4ed
ThinkMuk <b2000kang@gmail.com>
2025-01-21 00:12:58 +0900

    Feat: tailwind 모바일 커스텀 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
-// tailwind.config.js
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require('@nextui-org/react');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      screens: {
+        mobile: { max: '768px' }, // 768px 이하일 때 적용
       },
     },
   },
