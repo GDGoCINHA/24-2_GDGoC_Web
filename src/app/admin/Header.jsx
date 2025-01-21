a77c17c1187d8136f34843f6eccdcb73c2fa43ea
ThinkMuk <b2000kang@gmail.com>
2025-01-21 14:19:53 +0900

    Fix: 모바일 반응형 마진 및 커서 수정 #4 #6

--- src/app/admin/Header.jsx
+++ src/app/admin/Header.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <div className='flex flex-row select-none mt-[53px] mx-[96px] cursor-pointer mobile:justify-center'>
-      <div className='flex flex-row gap-x-[16px] w-fit'>
+    <div className='flex flex-row select-none mt-[53px] mx-[96px] mobile:justify-center mobile:mx-[0px]'>
+      <div className='flex flex-row gap-x-[16px] w-fit cursor-pointer'>
         <Image className='' src={gdgocIcon} alt='gdgocIcon' width={54} height={26} />
         <div className='text-white text-[14px] pt-[3px]'>
           <strong>GDGoC</strong> Inha univ.
