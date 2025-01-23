614f0f31ffed3b9cc552bf9ebd2f3c5f8ed64ac9
ThinkMuk <b2000kang@gmail.com>
2025-01-23 17:21:14 +0900

    Chore: config 파일 충돌 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-const {heroui} = require('@heroui/theme');
+const { heroui } = require('@heroui/theme');
 // tailwind.config.js
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require('@nextui-org/react');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/theme/dist/components/scroll-shadow.js"
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@heroui/theme/dist/components/scroll-shadow.js',
   ],
   theme: {
     extend: {
@@ -22,5 +22,5 @@ module.exports = {
       },
     },
   },
-  plugins: [nextui(),heroui()],
+  plugins: [nextui(), heroui()],
 };
