ce07cc1a2e24b42de1c417ef85c4c0af27ba57e5
Soyeon Kim <112332952+kaswhy@users.noreply.github.com>
2025-02-12 15:35:59 +0900

    fix: 메타태그 적용 안 되는 문제 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,31 +6,28 @@ import localFont from "next/font/local";
 const pretendard = localFont({
   src: "../../public/fonts/PretendardVariable.woff2",
   display: "swap",
-  variable: '--font-pretendard'
+  variable: "--font-pretendard"
 });
 
 const ocra = localFont({
   src: "../../public/fonts/OCRAExtended.woff2",
   display: "swap",
-  variable: '--font-ocra'
+  variable: "--font-ocra"
 });
 
 export const metadata = {
   title: "GDGoC INHA",
-  description: "GDGoC INHA",
-};
-
-export const viewport = {
-  width: "device-width",
-  initialScale: 1.0,
-  maximumScale: 1.0,
-  minimumScale: 1.0,
+  description: "개발자와 비개발자가 같이 성장하는 즐거움 with Google",
+  icons: {
+    icon: "/favicon.ico", 
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko" className={`${pretendard.variable} ${ocra.variable}`}>
       <head>
+        <meta name="description" content="개발자와 비개발자가 같이 성장하는 즐거움 with Google" />
         <script src="https://unpkg.com/type-hangul" async></script>
       </head>
       <body className={pretendard.className}>
