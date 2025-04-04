2d465cf08aba89c74558a742ecfcf7df3063efe9
Ruthgyeul <wingd4705@gmail.com>
2025-04-05 00:45:58 +0900

    Update: finalize of apply page; need edit the API calling later on #75

--- src/app/study/apply/page.jsx
+++ src/app/study/apply/page.jsx
@@ -1,50 +1,53 @@
 'use client';
 
-import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { Input, Button, Textarea, Spinner } from '@nextui-org/react';
+import Image from 'next/image';
 import axios from 'axios';
 import Header from '../Header';
-import { attendee } from "../mock/userData";
-import { studyDetail } from "../mock/studyData";
+import gdgocIcon from '@public/src/images/GDGoC_icon.png';
+import studyList from "../mock/studyData";
+import { user, attendee } from "../mock/userData";
 
-export default function ApplyPage() {
+export default function Apply() {
     const router = useRouter();
     const urlParams = useSearchParams();
     const studyTitle = urlParams.get('title');
     const [isLoading, setIsLoading] = useState(true);
     const [studyInfo, setStudyInfo] = useState(null);
 
-    // API 호출
-    // 이미 신청 받았을 경우 예외 처리 필요
+    // Call API
     useEffect(() => {
         const fetchData = async () => {
             try {
-                if (!studyTitle) {
-                    //throw new Error('Study title is missing');
+                // Check if studyTitle is provided
+                if (!studyTitle || studyTitle === "null") { // 타이틀이 없는 자, 너는 여길 지나갈수 없다!
                     router.push(`/study`);
+                    return;
                 }
 
-                const response = await axios.get(`https://temp.gdgocinha.site/studyData?title=${studyTitle}`);
+                if (process.env.NODE_ENV === 'development') {
+                    const studyData = studyList.data.studyList.find(study => study.title === studyTitle);
+                    setStudyInfo(studyData);
 
-                if (response.status === 200 && response.data) {
-                    setStudyInfo(response.data);
+                    const userApplication = attendee.data.applications.find(usr => usr.attendeeId === 12253956 && usr.studyId === studyData.id);
+                    if (userApplication) { // 당신은 이미 지원을 했다!
+                        router.push(`/study/detail?title=${encodeURIComponent(studyTitle)}`);
+                    }
                 } else {
-                    setStudyInfo(null);
+                    const { data: studyDataRes } = await axios.get(`https://temp.gdgocinha.site/studyData?page=1`);
+                    const studyData = studyDataRes.studyList.find(study => study.title === studyTitle);
+                    setStudyInfo(studyData);
+
+                    const thisUserId = 12253956;
+                    const { data: userApplications } = await axios.get(`https://temp.gdgocinha.site/atendee?studyId=${studyData.id}`);
+                    if (userApplications.applications.filter((application) => application.attendeeId === thisUserId).length > 0) {
+                        router.push(`/study/detail?title=${encodeURIComponent(studyTitle)}`);
+                    }
                 }
-                setIsLoading(false);
             } catch (error) {
-                //console.error('Error fetching study data:');
-
-                // remove when deploy
-                const data = studyDetail.data.filter(study => study.title === studyTitle);
-                if (data.length > 0) {
-                    setStudyInfo(data[0]);
-                } else {
-                    setStudyInfo(null);
-                }
-
-                setIsLoading(false); // remove when deploy
+                console.error('Error fetching study data');
             } finally {
                 setIsLoading(false);
             }
@@ -55,20 +58,29 @@ export default function ApplyPage() {
 
     // NEED EDIT
     const [formData, setFormData] = useState({
-        intro: "",
-        avaTime: "",
+        introduce: "",
+        activityTime: "",
     });
 
+    // onChange for form inputs
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
-    // NEED EDIT
-    const handleSubmit = (e) => {
+    // onSubmit for form
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(formData);
-        alert("신청이 완료!");
-        router.push(`/study/detail?title=${studyTitle}`);
+        try {
+            await axios.post(`https://temp.gdgocinha.site/studyWrite`, {
+                formData
+            });
+            alert("신청이 완료되었습니다!");
+
+            router.push(`/study/detail?title=${encodeURIComponent(studyTitle)}`);
+        } catch (error) {
+            console.error("Error submitting form");
+            alert("신청 중 오류가 발생했습니다. 다시 시도해주세요.");
+        }
     };
 
     return (
@@ -81,15 +93,36 @@ export default function ApplyPage() {
                 <>
                     {/* Header */}
                     <Header />
-                    <header className="relative flex flex-col select-none pt-[35px] px-[96px] mobile:px-[24px] mobile:justify-self-center">
-                        <h1 className="text-white text-2xl text-left mobile:text-center">
-                            {studyTitle} 스터디 신청하기
-                        </h1>
+                    <header className="relative flex flex-col select-none pt-[35px] px-[96px] mobile:px-[24px] items-center justify-center text-center">
+                        <div className="flex flex-col mobile:flex-col items-center gap-2 mt-4 mobile:mt-2">
+                            <div className="flex items-center gap-2">
+                                {studyInfo && Object.keys(studyInfo).length > 0 ? (
+                                    <Image
+                                        src={studyInfo.imagePath}
+                                        alt={`${studyTitle} Icon`}
+                                        width="30"
+                                        height="30"
+                                        className="object-contain mobile:w-[25px]"
+                                    />
+                                ) : (
+                                    <Image
+                                        src={gdgocIcon}
+                                        alt="gdgocIcon Icon"
+                                        width="30"
+                                        height="30"
+                                        className="object-contain mobile:w-[25px]"
+                                    />
+                                )}
+                                <h1 className="text-white text-2xl text-center mobile:text-lg">
+                                    {studyTitle} 신청하기
+                                </h1>
+                            </div>
+                        </div>
                     </header>
 
                     <div className="flex justify-center items-center mt-10 bg-black text-white">
                         <div className="w-full max-w-2xl px-6">
-                            {studyInfo ? (
+                            {studyInfo && Object.keys(studyInfo).length > 0 ? (
                                 <>
                                     {/* Form */}
                                     <form onSubmit={handleSubmit} className="space-y-6">
@@ -97,8 +130,8 @@ export default function ApplyPage() {
                                             자기소개 <span className="text-red-500">*</span>
                                         </label>
                                         <textarea
-                                            name="intro"
-                                            value={formData.intro}
+                                            name="introduce"
+                                            value={formData.introduce}
                                             onChange={handleChange}
                                             placeholder="자기 소개 및 활동 포부를 적어주세요."
                                             className="w-full bg-[#1f1f1f] border-none rounded-lg p-4 text-white h-40"
@@ -109,15 +142,15 @@ export default function ApplyPage() {
                                             활동 가능한 시간대 <span className="text-red-500">*</span>
                                         </label>
                                         <input
-                                            name="avaTime"
-                                            value={formData.avaTime}
+                                            name="activityTime"
+                                            value={formData.activityTime}
                                             onChange={handleChange}
                                             placeholder="활동 가능한 시간대를 모두 적어주세요. (ex. 월 17-19시, 화 15시~)"
                                             className="w-full bg-[#1f1f1f] border-none rounded-lg p-4 text-white"
                                             required
                                         />
 
-                                        {/* 제출 버튼 */}
+                                        {/* Submit */}
                                         <div className="flex justify-center pt-4">
                                             <Button
                                                 type="submit"
@@ -129,7 +162,12 @@ export default function ApplyPage() {
                                     </form>
                                 </>
                             ) : (
-                                <h1 className="text-center text-xl">스터디 신청 정보를 불러올 수 없습니다.</h1>
+                                <div className="w-full max-w-[800px] border border-white rounded-lg p-8 relative overflow-hidden">
+                                    <div className="text-white relative z-10">
+                                        <h1 className="text-center text-xl">스터디 신청 정보를 불러올 수 없습니다.</h1>
+                                        <p className="text-center">URL을 다시 확인해주세요.</p>
+                                    </div>
+                                </div>
                             )}
                         </div>
                     </div>
