24bbdf895542eb3dcdbf91c83571afc6be8875c9
Ruthgyeul <wingd4705@gmail.com>
2025-04-05 13:53:00 +0900

    Update: add useAuthenticatedApi for the API call

--- src/app/study/createStudy/page.jsx
+++ src/app/study/createStudy/page.jsx
@@ -1,19 +1,52 @@
 'use client';
 
-import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from "react";
-import { Image, Button, Spinner } from "@nextui-org/react";
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Button, Spinner } from "@nextui-org/react";
+import Image from 'next/image';
 import axios from "axios";
+import { useAuthenticatedApi } from '@/hooks/useAuthenticatedApi';
 import Header from '../Header';
+import studyList from "@/app/study/mock/studyData";
 
-export default function DetailPage() {
+export default function CreateStudy() {
     const router = useRouter();
+    const { apiClient } = useAuthenticatedApi();
     const urlParams = useSearchParams();
     const studyTitle = urlParams.get('title');
     const [isLoading, setIsLoading] = useState(false);
-    const [studyContent, setStudyContent] = useState(null);
+    const [studyInfo, setStudyInfo] = useState(null);
     const [isApplied, setIsApplied] = useState(false);
     const [imagePreview, setImagePreview] = useState(null);
+
+    // Call API
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Check if studyTitle is provided
+                if (!studyTitle || studyTitle === "null") { // 타이틀이 없는 자, 너는 여길 지나갈수 없다!
+                    router.push(`/study`);
+                    return;
+                }
+
+                if (process.env.NODE_ENV === 'development') {
+                    const studyData = studyList.data.studyList.find(study => study.title === studyTitle);
+                    setStudyInfo(studyData);
+                } else {
+                    const { data: studyDataRes } = await apiClient.get('/studyData?page=1');
+                    const studyData = studyDataRes.studyList.find(study => study.title === studyTitle);
+                    setStudyInfo(studyData);
+                }
+            } catch (error) {
+                console.error('Error fetching study data');
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [studyTitle]);
+
 
     const getCurrentDT = () => {
         const now = new Date();
@@ -21,17 +54,18 @@ export default function DetailPage() {
         return now.toISOString().slice(0, 16);
     };
 
+    // NEED EDIT
     const [formData, setFormData] = useState({
         title: "",
         introduce: "",
+        activityIntroduce: "",
         recruitStartTime: getCurrentDT(),
         recruitEndTime: "",
         activityStartTime: "",
         activityEndTime: "",
         expectedPlace: "",
         expectedTime: "",
-        activityIntroduce: "",
-        thumbnail: null
+        imagePath: null
     });
 
     const handleChange = (e) => {
@@ -51,16 +85,21 @@ export default function DetailPage() {
         }
     };
 
-    // NEED EDIT
-    const handleSubmit = (e) => {
+    // onSubmit for form
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(formData);
-        alert("신청이 완료!");
-        router.push(`/study/detail?title=${formData.title}`);
-    };
+        try {
+            await apiClient.post('/studyWrite', {
+                formData
+            });
 
-    const handleClick = () => {
-        //router.push(`/study/Detail?title=${}`);
+            alert("스터디 개설이 완료되었습니다!");
+
+            router.push(`/study/detail?title=${encodeURIComponent(formData.title)}`);
+        } catch (error) {
+            console.error("Error submitting form");
+            alert("신청 중 오류가 발생했습니다. 다시 시도해주세요.");
+        }
     };
 
     return (
@@ -73,10 +112,14 @@ export default function DetailPage() {
                 <>
                     {/* Header */}
                     <Header />
-                    <header className="relative flex flex-col select-none pt-[35px] px-[96px] mobile:px-[24px] mobile:justify-self-center">
-                        <h1 className="text-white text-2xl text-left mobile:text-center">
-                            신규 자율 스터디 개설하기
-                        </h1>
+                    <header className="relative flex flex-col select-none pt-[35px] px-[96px] mobile:px-[24px] items-center justify-center text-center">
+                        <div className="flex flex-col mobile:flex-col items-center gap-2 mt-4 mobile:mt-2">
+                            <div className="flex items-center gap-2">
+                                <h1 className="text-white text-2xl text-center mobile:text-lg">
+                                    신규 자율 스터디 개설하기
+                                </h1>
+                            </div>
+                        </div>
                     </header>
 
                     <div className="relative flex flex-col items-center justify-center w-full px-[96px] mobile:px-[24px] py-8">
@@ -194,7 +237,7 @@ export default function DetailPage() {
                                 </label>
                                 <input
                                     type="file"
-                                    name="thumbnail"
+                                    name="imagePath"
                                     accept="image/*"
                                     onChange={handleChange}
                                     className="w-full bg-[#1f1f1f] border-none rounded-lg p-4 text-white"
@@ -209,9 +252,9 @@ export default function DetailPage() {
                                             <Image
                                                 src={imagePreview}
                                                 alt="Thumbnail preview"
+                                                width="160"
+                                                height="160"
                                                 className="object-cover"
-                                                width="100%"
-                                                height="100%"
                                             />
                                         </div>
                                         <Button
@@ -219,8 +262,8 @@ export default function DetailPage() {
                                             size="sm"
                                             onPress={() => {
                                                 setImagePreview(null);
-                                                setFormData({...formData, thumbnail: null});
-                                                document.querySelector('input[name="thumbnail"]').value = '';
+                                                setFormData({...formData, imagePath: null});
+                                                document.querySelector('input[name="imagePath"]').value = '';
                                             }}
                                         >
                                             이미지 제거
