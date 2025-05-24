d0751c4632140840573530b3fdc6c916e1d3b791
pwc2002 <pwc2002@naver.com>
2025-05-25 00:20:48 +0900

    Design, Feat: 반응형 추가, mock 데이터 추가 변경

--- src/components/main/MainCarousel.jsx
+++ src/components/main/MainCarousel.jsx
@@ -35,13 +35,13 @@ export default function MainCarousel({ slides }) {
                 <div className="splide__slide" key={index}>
                 <div className="h-[616px] flex items-center justify-center rounded-lg relative overflow-hidden">
                     <div className="absolute inset-0 bg-black/50">
-                    <Image className='w-full h-full object-cover blur-lg opacity-50' src={slide.background} alt='event background' />
+                    <Image className='w-full h-full object-cover blur-lg opacity-50' width={100} height={100} src={slide.background} alt='event background' />
                     </div>
                     <div className="z-10 flex flex-row w-full h-full">
-                    <div className='flex justify-center items-center w-1/2 mobile:hidden'>
-                        <Image src={slide.poster} width={20} height={27} alt='event poster' className='w-[20vw] h-[46vh] object-contain' />
+                    <div className={slide.poster === 'none' ? 'hidden' : 'flex justify-center items-center w-1/2 mobile:hidden '}>
+                        <Image src={slide.poster} width={20} height={27} alt='event poster' className='w-[30vw] h-[56vh] object-contain' />
                     </div>
-                    <div className='flex justify-center items-start w-1/2 flex-col pr-10 mobile:pl-10 mobile:w-full'>
+                    <div className={`${slide.poster === 'none' ? 'w-full' : 'w-1/2'} flex justify-center items-start flex-col pr-10 mobile:pl-10 mobile:w-full px-10`}>
                         <div className="justify-start items-start flex flex-col gap-y-5">
                         <div className='flex flex-row gap-x-5'>
                             <div className='border-3 border-[#E94335] rounded-3xl text-[#E94335] font-bold text-xl flex items-center justify-center mobile:text-sm px-9 mobile:px-5 mobile:border-2'>
