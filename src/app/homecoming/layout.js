9fb700f66183a8cf7a272945491caf46c46ddfb1
JM Kim <106949557+CSE-Shaco@users.noreply.github.com>
2025-12-14 18:23:34 +0900

    fix(homecoming): modify typo and add meta img

--- src/app/homecoming/layout.js
+++ src/app/homecoming/layout.js
@@ -4,11 +4,28 @@ import Loader from '@/components/ui/common/Loader.jsx';
 export const metadata = {
     title: "Homecoming",
     description: "GDGoC INHA 제1회 홈커밍 데이 행사 안내 및 참여 페이지",
+
+    openGraph: {
+        title: "GDGoC INHA 제1회 홈커밍 데이",
+        description: "GDGoC INHA가 처음으로 선보이는 홈커밍 데이에 여러분을 초대합니다.",
+        url: "https://gdgocinha.com/homecoming",
+        siteName: "GDGoC INHA",
+        images: [
+            {
+                url: "/images/homecoming/meta_img.png", // 🔥 메타 이미지
+                width: 1200,
+                height: 630,
+                alt: "GDGoC INHA Homecoming Day",
+            },
+        ],
+        locale: "ko_KR",
+        type: "website",
+    },
 };
 
 export default function HomecomingLayout({ children }) {
     return (
-        <Suspense fallback={ <Loader /> }>
+        <Suspense fallback={<Loader />}>
             {children}
         </Suspense>
     );
